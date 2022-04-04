ee5c1cea5de72d33ba5f985ee2640c9b60f86704
Zombie <93808177+RpSide@users.noreply.github.com>
2022-04-04 10:48:42 -0500

    Create webRPDTA.js

---
+++ RpBrowser/web(RPDTA)/webRPDTA.js
@@ -0,0 +1,15 @@
+var url_string = window.location.href
+var url = new URL(url_string);
+var currenturl = url.searchParams.get("site")
+var endofurl = url.searchParams.get("int")
+
+funcion getfile(url){
+  let CONTENT
+fetch(url).then((r)=>{r.text().then((d)=>{CONTENT = d})})
+  return CONTENT
+}
+
+let indexCode = getfile(currenturl+"/index.RpDTA")
+let index = indexCode.replace("#","<").replace("!#",">")
+
+document.write(index)
