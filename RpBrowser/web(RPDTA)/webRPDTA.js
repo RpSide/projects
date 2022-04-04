404456012e9651bb00635a5d27e25db0956fdc20
Zombie <93808177+RpSide@users.noreply.github.com>
2022-04-04 14:03:49 -0500

    Update webRPDTA.js

--- RpBrowser/web(RPDTA)/webRPDTA.js
+++ RpBrowser/web(RPDTA)/webRPDTA.js
@@ -2,12 +2,13 @@ var url_string = window.location.href
 var url = new URL(url_string);
 var currenturl = url.searchParams.get("site")
 var endofurl = url.searchParams.get("int")
+let CONTENT
+fetch(currenturl+"index.RPDTA").then((r)=>{r.text().then((d)=>{
 
-  let CONTENT
-fetch(currenturl+"/index.RPDTA").then((r)=>{r.text().then((d)=>{CONTENT = d})})
+CONTENT = d
 
-
-let indexCode = CONTENT
-let index = indexCode.replace("#","<").replace("!#",">")
+let index = CONTENT.replace("#","<").replace("!#",">")
 
 document.write(index)
+
+})})
