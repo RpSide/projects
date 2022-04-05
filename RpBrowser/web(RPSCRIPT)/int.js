ad77a8db87746f1f4315dc3acefa9a4174daed03
Zombie <93808177+RpSide@users.noreply.github.com>
2022-04-05 15:21:40 -0500

    Update int.js

--- RpBrowser/web(RPSCRIPT)/int.js
+++ RpBrowser/web(RPSCRIPT)/int.js
@@ -5,7 +5,7 @@ var endofurl = url.searchParams.get("int")
 let CONTENT
 let index
 
-let data12
+let RSCRIPT_RPDTA
 // As with JSON, use the Fetch API & ES6
 fetch(document.domain+"/RPSCRIPT/RPSCRIPT_RPDTA")
   .then(response => response.text())
@@ -14,7 +14,7 @@ fetch(document.domain+"/RPSCRIPT/RPSCRIPT_RPDTA")
   	data12 = data
   });
 
-let data12
+let RSCRIPT_NOTIFY
 // As with JSON, use the Fetch API & ES6
 fetch(document.domain+"/RPSCRIPT/RPSCRIPT_NOTIFY.JS")
   .then(response => response.text())
@@ -23,7 +23,7 @@ fetch(document.domain+"/RPSCRIPT/RPSCRIPT_NOTIFY.JS")
   	data12 = data
   });
 
-let data12
+let RSCRIPT_RDOC
 // As with JSON, use the Fetch API & ES6
 fetch(document.domain+"/RPSCRIPT/RPSCRIPT_RDOC.JS")
   .then(response => response.text())
@@ -42,7 +42,7 @@ fetch(currenturl+"index.RPSCRIPT").then((r)=>{r.text().then((d)=>{
 
 CONTENT = d
 
-index = CONTENT.replaceAll("*", "let").replaceAll("%INSERT @RPSCRIPT_RDOC",getdata(document.domain + "/side-projects/RPSCRIPT_RDOC.JS")).replaceAll("%INSERT @RPSCRIPT_NOTIFY",getdata(document.domain + "/side-projects/RPSCRIPT_NOTIFY.JS")).replaceAll("%INSERT @RSCRIPT_RPDTA",getdata(document.domain + "/side-projects/RPSCRIPT_RPDATA.JS"))
+index = CONTENT.replaceAll("*", "let").replaceAll("%INSERT @RPSCRIPT_RDOC",RPSCRIPT_RDOC).replaceAll("%INSERT @RPSCRIPT_NOTIFY",RPSCRIPT_NOTIFY).replaceAll("%INSERT @RSCRIPT_RPDTA",RPSCRIPT_RPDTA)
 
 document.write(`
 <script>
