fd77682134cac4171bd4be3c4173d8acf1ed6906
Zombie <93808177+RpSide@users.noreply.github.com>
2022-04-06 15:46:26 -0500

    Update webRPDTA.js

--- rpweb/RPDTA/webRPDTA.js
+++ rpweb/RPDTA/webRPDTA.js
@@ -1,6 +1,6 @@
 var url_string = window.location.href
 var url = new URL(url_string);
-var currenturl = url.searchParams.get("site")
+var currenturl = url.searchParams.get("site").replace("rpside://")
 var endofurl = url.searchParams.get("int")
 let CONTENT
 fetch(currenturl+"index.RPDTA").then((r)=>{r.text().then((d)=>{
